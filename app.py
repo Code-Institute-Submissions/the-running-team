0afed81ddde9f029f02142f54f0bc20b03ba7b36
Thor-Ole Øby <punkthor@gmail.com>
2020-08-13 20:51:45 +0200

    Added additional imports.

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "It's working!"
 
+
 if __name__ == "__main__":
-    app.run(host = os.environ.get("IP"),
-    port = int(os.environ.get("PORT")),
-    debug = True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
