f7a9e0162abd1b6278bcdee7eacd29c731e66b5d
Thor-Ole Øby <punkthor@gmail.com>
2020-08-18 19:59:54 +0200

    Added route to register.html.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,6 +19,11 @@ mongo = PyMongo(app)
 def get_members():
     members = mongo.db.team_members.find()
     return render_template("members.html", members=members)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
