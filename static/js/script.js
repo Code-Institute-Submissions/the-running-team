45f313b41dc7656a5633c7b0863b882196ac59f7
Thor-Ole Øby <punkthor@gmail.com>
2020-08-20 20:22:20 +0200

    Added line of code to enable form validation on materialize selects.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
+    $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: 'absolute'});
   });
