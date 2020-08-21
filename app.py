4439cfac5cfa16ba64314f1c9ef217a8975c3907
Thor-Ole Øby <punkthor@gmail.com>
2020-08-21 20:38:03 +0200

    Added first part of login functionality. If the user exists, the login is now functional.

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_members")
 def get_members():
@@ -23,17 +24,18 @@ def get_members():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    print("register in process")
     if request.method == "POST":
         existing_user = mongo.db.team_members.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-    
+
         new_member = {
             "username": request.form.get("username").lower(),
-            "password": request.form.get("password").lower(),
+            "password": generate_password_hash(request.form.get("password")),
             "first_name": request.form.get("first_name").lower(),
             "last_name": request.form.get("last_name").lower(),
             "fitness": request.form.get("fitness"),
@@ -48,11 +50,28 @@ def register():
         flash("Registration Successful!")
     return render_template("register.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    print("loggin in")
     if request.method == "POST":
-        existing_user = mongo.db.team_members.find_one({"username": request.form.get("username").lower})
+        print("it's a post method")
+        existing_user = mongo.db.team_members.find_one(
+            {"username": request.form.get("username").lower()})
+        print(existing_user)
+        if existing_user:
+            print("this is an exisiting user")
+            print(check_password_hash(existing_user["password"], request.form.get("password")))
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                print("Matching password")
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("get_members"))
+
+        else:
+            return redirect(url_for("login"))
     return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
