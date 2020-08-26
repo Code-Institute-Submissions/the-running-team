112ef07e27d34848ef660a4796f763033d9825d1
Thor-Ole Øby <punkthor@gmail.com>
2020-08-26 22:13:53 +0200

    Removed blank space.

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-    $('.modal').modal();
-    /*
-    This line enables validation on materialize selects by displaying the otherwise hidden select element.
-    The idea was taken from stackoverflow user "Imran Saleem."
-    */ 
-    $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: 'absolute'});
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('select').formSelect();
+  $('.modal').modal();
+  $('.tabs').tabs();
+  /*
+  This line enables validation on materialize selects by displaying the otherwise hidden select element.
+  The idea was taken from stackoverflow user "Imran Saleem."
+  */
+  $("select[required]").css({ display: "block", height: 0, padding: 0, width: 0, position: 'absolute' });
+});
