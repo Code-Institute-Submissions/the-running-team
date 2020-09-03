441f6b1b5123c0b17e2ef4060058947aec226fe7
Thor-Ole Øby <punkthor@gmail.com>
2020-09-03 11:10:14 +0200

    Added function that sets color to prog bar according to its value.

--- static/js/script.js
+++ static/js/script.js
@@ -12,4 +12,41 @@ $(document).ready(function () {
   The idea was taken from stackoverflow user "Imran Saleem."
   */
   $("select[required]").css({ display: "block", height: 0, padding: 0, width: 0, position: 'absolute' });
+
+  /*
+  This function iterates through all the progressbars, gets its data-value, and
+  sets a class(color) to the progressbar according to the data-value.
+  */
+  function setProgBarColor() {
+    let i = 0;
+    let progBar = document.getElementsByClassName("determinate")
+    while (i < progBar.length) {
+
+      let progBar = document.getElementsByClassName("determinate")[i];
+      let progValue = progBar.getAttribute("data-value");
+      console.log(progValue);
+      if (progValue == "100%") {
+        console.log("setting color")
+        $(progBar).addClass("light-blue darken-4");
+      }
+      else if (progValue == "80%") {
+        console.log("setting color")
+        $(progBar).addClass("light-blue darken-1");
+      }
+      else if (progValue == "60%") {
+        console.log("setting color")
+        $(progBar).addClass("light-blue lighten-1");
+      }
+      else if (progValue == "40%") {
+        console.log("setting color")
+        $(progBar).addClass("light-blue lighten-2");
+      }
+      else {
+        $(progBar).addClass("light-blue lighten-3");
+      }
+      i++;
+    }
+  }
+  setProgBarColor();
 });
+
