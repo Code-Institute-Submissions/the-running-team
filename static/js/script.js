ea3fa5e0c52d6d2e3e761df461c8825faeaac62d
Thor-Ole Øby <punkthor@gmail.com>
2020-09-07 14:57:19 +0200

    Added jquery for collapsible.

--- static/js/script.js
+++ static/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     autoClose: true
   });
   $('.timepicker').timepicker();
+  $('.collapsible').collapsible();
   /*
   This line enables validation on materialize selects by displaying the otherwise hidden select element.
   The idea was taken from stackoverflow user "Imran Saleem."
