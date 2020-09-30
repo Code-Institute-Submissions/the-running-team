f735265d36af21a66e1c24b4632766c02a09e18d
Thor-Ole Øby <punkthor@gmail.com>
2020-09-30 10:16:31 +0200

    Reduced amount of pixels scrolled before displaying back to top button.

--- static/js/script.js
+++ static/js/script.js
@@ -62,7 +62,7 @@ $(document).ready(function () {
   window.onscroll = function () { scrollFunction() };
 
   function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
       mybutton.style.display = "block";
     } else {
       mybutton.style.display = "none";
