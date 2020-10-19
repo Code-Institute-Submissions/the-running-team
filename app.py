9d3bd70935bc18a11c5ca191f8d28d8f07faad8a
Thor-Ole Øby <punkthor@gmail.com>
2020-10-19 15:32:37 +0200

    Set debug=False

--- app.py
+++ app.py
@@ -527,4 +527,4 @@ def toggle_admin():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
